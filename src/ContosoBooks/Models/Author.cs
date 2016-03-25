36d3e796e2836a623d495a1116f23f12e4848806
Ramon Aseniero <raseniero@hotmail.com>
2016-03-24 20:44:25 -0700

    Initial Commits

---
+++ src/ContosoBooks/Models/Author.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoBooks.Models
+{
+    public class Author
+    {
+        [ScaffoldColumn(false)]
+        public int AuthorID { get; set; }
+
+        [Required]
+        [Display(Name ="Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
